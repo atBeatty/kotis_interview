aaa31ba17b7536f836bdc83f97b2379362e0b26c
Jay Fredlund <jayfredlund@gmail.com>
2021-12-16 20:33:34 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Spin up the Rails server and access the full instructions on the root path.
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+
+  def index
+    respond_to do |format|
+      format.json { return head :no_content }
+      format.any { render :index, layout: 'welcome_index' }
+    end
+  end
+
+  def task_one
+  end
+
+  def task_two
+  end
+
+end
